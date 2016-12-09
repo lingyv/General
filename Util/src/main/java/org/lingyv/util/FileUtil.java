ddca1e852d9de00857cfc874e8b235838d9ee09f
lingyv <lingyv@gmail.com>
2016-12-09 16:19:52 +0800

    File I/O

--- Util/src/main/java/org/lingyv/util/FileUtil.java
+++ Util/src/main/java/org/lingyv/util/FileUtil.java
@@ -3,8 +3,7 @@ package org.lingyv.util;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,6 +11,95 @@ import java.util.List;
  * Created by lingyv on 2016/11/8.
  */
 public class FileUtil {
+
+    /**
+     * 读取文件
+     * 将文件内容保存在List<String>对象中
+     *
+     * @param filePath
+     * @return
+     * @throws IOException
+     */
+    public static List<String> fileReader(String filePath) throws IOException {
+        File file = new File(filePath);
+        List<String> fileContent = new ArrayList<String>();
+        if (file.exists()) {
+            try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+                String lineContent = null;
+                //读取文件的每一行，并添加到List容器中
+                while ((lineContent = reader.readLine()) != null) {
+                    //加上换行符(读的过程中换行符会被去除掉)
+                    //fileContent.add(lineContent+"\n");
+                    fileContent.add(lineContent);
+                }
+            }
+        } else {
+            throw new IOException("文件 " + filePath + " 不存在");
+        }
+
+        return fileContent;
+    }
+
+    /**
+     * 将数据写入文件
+     * 如果文件不存在，则创建文件
+     * 如果文件存在且有内容，则会删掉原有内容再写入
+     *
+     * @param dirPath
+     * @param fileName
+     * @param fileContent
+     * @return
+     * @throws IOException
+     */
+    public static boolean fileWriter(String dirPath, String fileName, List<String> fileContent) throws IOException {
+        return writer(dirPath, fileName, fileContent, false);
+    }
+
+    /**
+     * 将数据追加写入文件
+     * 如果文件不存在，则创建文件
+     *
+     * @param dirPath
+     * @param fileName
+     * @param fileContent
+     * @throws IOException
+     */
+    public static boolean fileAppend(String dirPath, String fileName, List<String> fileContent) throws IOException {
+        return writer(dirPath, fileName, fileContent, true);
+    }
+
+    /**
+     * 将数据写入文件
+     * 如果文件不存在，则创建文件
+     * @param dirPath
+     * @param fileName
+     * @param fileContent
+     * @param append  --> 是否保留原文件内容
+     * @return
+     * @throws IOException
+     */
+    private static boolean writer(String dirPath, String fileName, List<String> fileContent, boolean append) throws IOException {
+        boolean result = false;
+        if (createDir(dirPath)) {
+            String filePath = dirPath + "/" + fileName;
+            try (FileWriter fileWriter = new FileWriter(filePath,append);
+                 BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+                 PrintWriter writer = new PrintWriter(bufferedWriter)) {
+                if (fileContent != null && !fileContent.isEmpty()) {
+                    for (String content : fileContent) {
+                        writer.println(content);
+                    }
+                    result = true;
+                } else {
+                    throw new IOException("没有要写入的内容");
+                }
+            }
+        } else {
+            throw new IOException("文件夹" + dirPath + "不存在");
+        }
+
+        return result;
+    }
 
     /**
      * 将文件路径转换成真实的保存路径
@@ -106,6 +194,16 @@ public class FileUtil {
         }
 
         return fileNames;
+    }
+
+
+    public static boolean createDir(String dirPath) {
+        boolean success = true;
+        File file = new File(dirPath);
+        if (!file.exists()) {
+            success = file.mkdirs();
+        }
+        return success;
     }
 
     /**
